733222ebf7486710a8febe5625837e3af9d028fc
fearanha <felipearanha.c@gmail.com>
2023-07-20 12:48:46 -0300

    insert

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,19 @@
 import fastify from "fastify";
+import crypto from 'node:crypto'
 import { knex } from "./database"
 
 const app = fastify()
 
 app.get('/hello', async() => {
-  const tables = await knex('sqlite_schema').select('*')
+  const user = await knex('users').insert({
+    id: crypto.randomUUID(),
+    name: 'tester',
+    email: 'tester@email.com',
+    password: '123',
+  })
+  .returning('*')
   
-  return tables
+  return user
 })
 
 app.listen({
