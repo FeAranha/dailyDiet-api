b909b16f5dbdfbafbd2b7989f7f956d41d477a49
fearanha <felipearanha.c@gmail.com>
2023-07-20 13:19:31 -0300

    zod

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import fastify from "fastify";
-import crypto from 'node:crypto'
+import { env } from "./env"
 import { knex } from "./database"
 
 const app = fastify()
@@ -11,7 +11,7 @@ app.get('/hello', async() => {
 })
 
 app.listen({
-  port: 3333,
+  port: env.PORT,
 }).then(() => {
     console.log('HTTP server Running!')
   })
